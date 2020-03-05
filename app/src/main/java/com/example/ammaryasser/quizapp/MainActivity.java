674afc74a67ad2f088a1f3dfc3ca547cd990e993
Ammar Yasser AllaiThy <AmmarYasserAllaiThy@gmail.com>
2020-03-05 09:14:02 +0200

    Integrate App with changes

--- app/src/main/java/com/example/ammaryasser/quizapp/MainActivity.java
+++ app/src/main/java/com/example/ammaryasser/quizapp/MainActivity.java
@@ -4,6 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -21,12 +25,40 @@ public class MainActivity extends AppCompatActivity {
     private RadioButton ansRadio_1;
     private RadioButton ansRadio_2;
     private RadioButton ansRadio_3;
-    private RadioButton ansRadio_4;
+    private CheckBox checkBox;
+    private EditText et;
 
+    private QuizFactory quizFactory;
     private QuizCountDown countDown;
     private int id = 0;
     private int correctRadioId;
     private int correctAnswersNo = 0;
+
+    private final RadioGroup.OnCheckedChangeListener groupListener = (group, checkedId) -> checkBox.setChecked(checkedId == -1);
+    private final CheckBox.OnCheckedChangeListener boxListener = (buttonView, isChecked) -> {
+        et.setEnabled(isChecked);
+        if (isChecked) {
+            radioGroup.clearCheck();
+//            countDown.cancel();
+        }
+//        else countDown.start();
+        /* fixme...
+             It should pause the countdown and resume it when submit but it doesn't have a pause()
+             And if i used cancel() and then start() it
+             It starts a new countdown.
+         */
+    };
+    private final View.OnClickListener submitListener = view -> {
+        int id = radioGroup.getCheckedRadioButtonId();
+        if (checkBox.isChecked() || id != -1) {
+            Toast.makeText(this,
+                    (id == correctRadioId ? "✔ Correct" : "✘ Wrong") + " answer",
+                    Toast.LENGTH_SHORT
+            ).show();
+            if (id == correctRadioId) correctAnswersNo++;
+            countDown.onFinish();
+        } else Toast.makeText(this, "No answer selected", Toast.LENGTH_SHORT).show();
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +72,15 @@ public class MainActivity extends AppCompatActivity {
         ansRadio_1 = findViewById(R.id.ansRadio_1);
         ansRadio_2 = findViewById(R.id.ansRadio_2);
         ansRadio_3 = findViewById(R.id.ansRadio_3);
-        ansRadio_4 = findViewById(R.id.ansRadio_4);
+        checkBox = findViewById(R.id.checkbox);
+        et = findViewById(R.id.et);
+        Button submitBTN = findViewById(R.id.submit_btn);
+
+        radioGroup.setOnCheckedChangeListener(groupListener);
+        checkBox.setOnCheckedChangeListener(boxListener);
+        submitBTN.setOnClickListener(submitListener);
+
+        quizFactory = new QuizFactory(this);
     }
 
     @Override
@@ -52,8 +92,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 
-        QuizFactory.reset();
+        quizFactory.reset();
         int duration = 10000;
         countDown = new QuizCountDown(duration, 10) {
             public void onFinish() {
@@ -75,36 +116,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private boolean showNextQuiz() {
-        Quiz quiz = QuizFactory.getNext();
+        Quiz quiz = quizFactory.getNext();
 
         if (quiz != null) {
             nTV.setText(getString(R.string.question_id, ++id, 10));
             qTV.setText(quiz.getQuestion());
             radioGroup.clearCheck();
+            checkBox.setChecked(false);
+            et.setText("");
             ansRadio_1.setText(quiz.getAnswers()[0]);
             ansRadio_2.setText(quiz.getAnswers()[1]);
             ansRadio_3.setText(quiz.getAnswers()[2]);
-            ansRadio_4.setText(quiz.getAnswers()[3]);
             correctRadioId =
                     quiz.getCorrectAnswerId() == 0 ? ansRadio_1.getId()
                             : quiz.getCorrectAnswerId() == 1 ? ansRadio_2.getId()
-                            : quiz.getCorrectAnswerId() == 2 ? ansRadio_3.getId()
-                            : ansRadio_4.getId();
+                            : ansRadio_3.getId();
             return true;
         }
         return false;
-    }
-
-    public void submit(View view) {
-        int id = radioGroup.getCheckedRadioButtonId();
-        if (id != -1) {
-            Toast.makeText(this,
-                    (id == correctRadioId ? "✔ Correct" : "✘ Wrong") + " answer",
-                    Toast.LENGTH_SHORT
-            ).show();
-            if (id == correctRadioId) correctAnswersNo++;
-            countDown.onFinish();
-        } else Toast.makeText(this, "No answer selected", Toast.LENGTH_SHORT).show();
     }
 
 
@@ -122,7 +151,6 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onFinish() {
-            progressBar.setProgress(0);
         }
     }
 }
