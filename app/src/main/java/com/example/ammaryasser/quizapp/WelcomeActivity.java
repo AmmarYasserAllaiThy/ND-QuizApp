a86ad5a02f889f6ff94fa9b29a7988f4d2141871
Ammar Yasser AllaiThy <AmmarYasserAllaiThy@gmail.com>
2020-03-05 09:10:18 +0200

    Wrap in ScrollView

--- app/src/main/java/com/example/ammaryasser/quizapp/WelcomeActivity.java
+++ app/src/main/java/com/example/ammaryasser/quizapp/WelcomeActivity.java
@@ -8,15 +8,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class WelcomeActivity extends AppCompatActivity {
 
+    private final View.OnClickListener startListener = view -> startActivity(new Intent(this, MainActivity.class));
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-    }
-
-    public void start(View view) {
-        startActivity(new Intent(this, MainActivity.class));
+        findViewById(R.id.start_btn).setOnClickListener(startListener);
     }
 }
-
-//Image by <a href="https://pixabay.com/users/mohamed_hassan-5229782/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=3644414">mohamed Hassan</a> from <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=3644414">Pixabay</a>
